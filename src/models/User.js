59b4b65789c0eb2d1df1bd59f1715fa753fcdd52
Simon Paul <samonpaul06@gmail.com>
2023-03-11 22:31:10 +0530

    recipe routes

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,8 @@ import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "recipes" }]
 })
 
 export const UserModel = mongoose.model("users", userSchema) 
